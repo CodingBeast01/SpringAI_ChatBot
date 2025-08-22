6774a2c8661d14cdf090e6ee35584326fb1d11c4
Kritiman Singh <kritimansingh123@gmail.com>
2025-08-22 23:30:36 +0530

    Initial commit

---
+++ src/main/java/com/ai/gemini_chat/controller/AiController.java
@@ -0,0 +1,27 @@
+package com.ai.gemini_chat.controller;
+
+import com.ai.gemini_chat.service.QnaService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/qna")
+public class AiController {
+
+    private final QnaService qnaService;
+
+    @RequestMapping("/api/v1/chat")
+    public ResponseEntity<String> askQuestion(@RequestBody Map<String,String> payload){
+
+        String question = payload.get("question");
+        String answer =  qnaService.getAnswer(question);
+        return ResponseEntity.ok(answer);
+    }
+}
---
+++ src/main/java/com/ai/gemini_chat/service/QnaService.java
@@ -0,0 +1,13 @@
+package com.ai.gemini_chat.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QnaService {
+
+    public String getAnswer(String question) {
+// construct the request PAyload
+
+        return "";
+    }
+}
